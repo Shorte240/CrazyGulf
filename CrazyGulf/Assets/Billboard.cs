5a99fa6d8f0efb0e4d893e2c3fd08b998e0dae44
Stuart Pentelow <Stuartpentelow@outlook.com>
2019-12-16 17:48:30 +0000

    Radar

---
+++ CrazyGulf/Assets/Billboard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Camera maincam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maincam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.localRotation = maincam.transform.rotation;
+    }
+}
---
+++ CrazyGulf/Assets/ForcePosition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForcePosition : MonoBehaviour
+{
+    public GameObject targetPosition;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = targetPosition.transform.position + offset;
+    }
+}
