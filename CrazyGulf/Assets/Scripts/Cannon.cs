72cc052a56a88068ac8cf545596dd2ea6f6e8b7b
Shorte240 <fraserbarker@hotmail.com>
2019-12-16 10:27:03 +0000

    Added plane prototype

---
+++ CrazyGulf/Assets/Scripts/Cannon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public GameObject bullet_prefab;
+    public Transform bullet_spawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Fire the cannon
+        if (Input.GetMouseButton(0))
+        {
+            // Play sound
+            // Spawn bullets
+            GameObject bullet = Instantiate(bullet_prefab, bullet_spawn.position, bullet_spawn.transform.rotation);
+            Rigidbody bullet_rb = bullet.GetComponent<Rigidbody>();
+            bullet_rb.AddForce(bullet.transform.forward * 75000.0f * Time.deltaTime);
+            // Stuff
+        }
+    }
+}
