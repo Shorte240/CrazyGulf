f7e2b7245342aec3569ff298131d4d1ff763c3a1
Stuart Pentelow <Stuartpentelow@outlook.com>
2019-12-16 15:14:13 +0000

    AA Gun

--- CrazyGulf/Assets/AI_Gun.cs
+++ CrazyGulf/Assets/AI_Gun.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class AI_Gun : MonoBehaviour
 {
+    public GameObject bulletPrefab;
+    public float forceModifier = 150000.0f;
+    public GameObject firepoint;
     private GameObject plane;
-    private Vector3 firePoint;
+    float timer = 0.0f;
+    float fireTimer = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        plane = GameObject.FindGameObjectWithTag("plane");
-        firePoint = GetComponentInChildren<Transform>().position;
+        plane = GameObject.FindGameObjectWithTag("Plane");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       timer += Time.deltaTime;
+       if(timer > fireTimer)
+        {
+            timer = 0.0f;
+            Vector3 directionalVec = plane.transform.position - gameObject.transform.position;
+           // directionalVec.Normalize();
+            GameObject bullet = Instantiate(bulletPrefab,firepoint.transform.position, bulletPrefab.transform.rotation);
+            Rigidbody bullet_rb = bullet.GetComponent<Rigidbody>();
+            bullet_rb.AddForce(directionalVec * forceModifier * Time.deltaTime);
+        }
     }
 }
