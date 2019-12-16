cedd3690da758ea9c6692756faaaf24d1f0c3e64
Stuart Pentelow <Stuartpentelow@outlook.com>
2019-12-16 14:45:18 +0000

    Truck AI

---
+++ CrazyGulf/Assets/AI_Truck.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI_Truck : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public GameObject[] points = new GameObject[2];
+    int pointIndex = 0;
+    public Vector3 rotation = new Vector3(0,0,0);   // Forced rotation axis.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(points[0].transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Switch to other point if we find that the distance to the destination point is less than 0.1f.
+        if(agent.remainingDistance < 0.1f)
+        {
+            if(pointIndex == 0)
+            {
+                pointIndex = 1;
+            }
+            else
+            {
+                pointIndex = 0;
+            }
+            agent.SetDestination(points[pointIndex].transform.position);
+        }
+        // Force rotation of the truck
+        gameObject.transform.eulerAngles = rotation;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "bullet")
+        {
+            gameObject.SetActive(false);
+
+        }
+    }
+}
